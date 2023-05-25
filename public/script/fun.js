e29e37ec0c2909cd44a4deaf94d3d1efb402cf0f
helio Cunha <heliocunha.souza@gmail.com>
2023-05-25 16:24:47 -0300

    add nova pagina sobre

--- public/script/fun.js
+++ public/script/fun.js
@@ -1,12 +1,38 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
+const nomeInput = document.querySelector('input[type="text"]');
+const nomeError = document.querySelector('.nome-error');
+
 signUpButton.addEventListener('click', () => {
     container.classList.add("right-panel-active");
 });
 signInButton.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
-});
+})
+
+function validaForm() {
+            var nome = document.getElementById("nome").value;
+            var email = document.getElementById("email").value;
+            var telefone = document.getElementById("telefone").value;
+            var senha = document.getElementById("senha").value;
+            
+            if (nome.length < 4) {
+                alert("Nome deve ter pelo menos 4 caracteres!");
+                return false;
+            }
+            
+            if (senha.length < 4) {
+                alert("Senha deve ter pelo menos 4 caracteres!");
+                return false;
+            }
+            
+            alert("Cadastro realizado com sucesso!");
+            return true;
+        }
+
+
+
 
 
 
--- router.js
+++ router.js
@@ -3,6 +3,7 @@ const express = require("express")
 //cria o roteador 
 const router = express.Router("router")
 
+
 //definindo rotas para o roteador
 router.get("/", (req, res)=> {
     return res.sendFile(__dirname + "/views/index.html")
@@ -10,6 +11,24 @@ router.get("/", (req, res)=> {
 router.get("/views/second.html", (req, res)=> {
     return res.sendFile(__dirname + "/views/second.html")
 })
+
+router.get("/public/img/img.jpg", (req, res)=> {
+    return res.sendFile(__dirname + "/public/img/img.jpg")
+})
+
+router.get("/views/blog.html", (req, res)=> {
+    return res.sendFile(__dirname + "/views/blog.html")
+})
+router.get("/views/index.html", (req, res)=> {
+    return res.sendFile(__dirname + "/views/index.html")
+})
+router.get("/views/sobre.html", (req, res)=> {
+    return res.sendFile(__dirname + "/views/sobre.html")
+})
+
+
+
+
 
 //exportando roteador
 module.exports = router
